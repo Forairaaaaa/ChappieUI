c9ada6fb1209a775ee9f09a05bba5c864da7139b
Forairaaaaa <applesyqd@outlook.com>
2023-03-14 23:09:41 +0800

    add launcher

---
+++ SquarelineProject/Launcher/ChappieUI_Launcher_events.py
@@ -0,0 +1,16 @@
+
+def MenuIconUpdate(event_struct):
+    return
+
+
+def ClockPanelClicked(event_struct):
+    return
+
+
+def AppIconClicked(event_struct):
+    return
+
+
+def AppListValueChanged(event_struct):
+    return
+
